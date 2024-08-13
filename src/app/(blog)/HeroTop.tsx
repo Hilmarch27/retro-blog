dc373f994f7d824cf97f455e97c377142fd41a8a
ricokurnia12 <ricokurnia19@gmail.com>
2024-08-13 17:00:58 +0700

    config mdx

--- src/app/(blog)/HeroTop.tsx
+++ src/app/(blog)/HeroTop.tsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import ArticleCard from '@/components/custom/ArticleCard'
-import ArticleCardHorizontal from '@/components/custom/HorizontalCard'
-import HeroCard from '@/components/custom/HeroCard'
-import Heroimg from '../assets/hero-img.png'
-import Image from 'next/image'
+import React from "react";
+import ArticleCard from "@/components/custom/ArticleCard";
+import ArticleCardHorizontal from "@/components/custom/HorizontalCard";
+import HeroCard from "@/components/custom/HeroCard";
+import Heroimg from "../../assets/heroimg.png";
+import Image from "next/image";
 const HeroTop = () => {
-    return (
-        <div className='grid grid-cols-12 lg:mt-12'>
-            <div className='col-span-12 lg:col-span-8'>
-                <ArticleCardHorizontal
-                    title="Implement JWT Auth in NestJs"
-                    description="What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                    imgSrc={`https://static-00.iconduck.com/assets.00/nestjs-icon-2048x2040-3rrvcej8.png`}
-                />
-                <ArticleCardHorizontal
-                    title="Implement JWT Auth in NestJs"
-                    description="What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                    imgSrc={`https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png`}
-                />
-            </div>
-            <div className='col-span-12 lg:col-span-4'>
-                <HeroCard
-                    title="Power of Javascript"
-                    author="Rico Kurnia"
-                    date="12 Juni 2024"
-                    content="Learn about the reduce method in JavaScript arrays and how it can optimize code for speed and efficiency."
-                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
-                />
-                {/* <Image className='w-full h-full' src={Heroimg} /> */}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="grid grid-cols-12 lg:mt-12">
+      <div className="col-span-12 lg:col-span-8">
+        {/* kontent mdx start */}
+        <h1 className="text-2xl font-semibold">Hello Im Rico Kurnia </h1>
+        {/* kontent mdx end */}
+      </div>
+      <div className="col-span-12 lg:col-span-4">
+        {/* <HeroCard
+          title="Power of Javascript"
+          author="Rico Kurnia"
+          date="12 Juni 2024"
+          content="Learn about the reduce method in JavaScript arrays and how it can optimize code for speed and efficiency."
+          imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+        /> */}
+        <Image
+          alt="Hero Image"
+          height={1000}
+          width={1000}
+          className="w-full h-full"
+          src={Heroimg}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default HeroTop
+export default HeroTop;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,103 @@
-import React from 'react'
+import React from "react";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import HeroTop from './(blog)/HeroTop'
-import ArticleCard from '@/components/custom/ArticleCard'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import HeroTop from "./(blog)/HeroTop";
+import ArticleCard from "@/components/custom/ArticleCard";
+const articles = [
+  {
+    title: "Power of JavaScript",
+    author: "Rico Kurnia",
+    date: "12 Juni 2024",
+    content: "Learn about the reduce method in JavaScript arrays and how it.",
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  {
+    title: "Understanding React",
+    author: "Jane Doe",
+    date: "20 Mei 2024",
+    content: "A comprehensive guide to the fundamentals of React.",
+    imgSrc: "https://reactjs.org/logo-og.png",
+  },
+  // Add 6 more articles here...
+];
 
 const MainPage = () => {
-    return (
-        <section>
-            {/* <HeroSection/> */}
-            {/* top hero */}
-            <HeroTop />
+  return (
+    <section>
+      {/* <HeroSection/> */}
+      {/* top hero */}
+      <HeroTop />
 
-            {/* end of top hero */}
-            <div className='flex gap-4 justify-center'>
-                <Tabs defaultValue="account" className="w-full">
-                    <TabsList>
-                        <TabsTrigger value="account">Account</TabsTrigger>
-                        <TabsTrigger value="password">Password</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="account">
-                        <div className='grid grid-cols-12 gap-8'>
-                            <div className='col-span-4'>
-                                <ArticleCard title="Power of Javascript"
-                                    author="Rico Kurnia"
-                                    date="12 Juni 2024"
-                                    content="Learn about the reduce method in JavaScript arrays and how it."
-                                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" />
-                            </div>
-                            <div className='col-span-4'>
-                                <ArticleCard title="Power of Javascript"
-                                    author="Rico Kurnia"
-                                    date="12 Juni 2024"
-                                    content="Learn about the reduce method in JavaScript arrays and how it can  lorem10 iejsif sefshefe fefe optimize code for speed and efficiency."
-                                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" />
-                            </div>
-                            <div className='col-span-4'>
-                                <ArticleCard title="Power of Javascript"
-                                    author="Rico Kurnia"
-                                    date="12 Juni 2024"
-                                    content="Learn about the reduce method in JavaScript arrays and how it can optimize code for speed and efficiency."
-                                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" />
-                            </div>
-                            <div className='col-span-4'>
-                                <ArticleCard title="Power of Javascript"
-                                    author="Rico Kurnia"
-                                    date="12 Juni 2024"
-                                    content="Learn about the reduce method in JavaScript arrays and how it can optimize code for speed and efficiency."
-                                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" />
-                            </div>
-
-                        </div>
-                    </TabsContent>
-                    <TabsContent value="password">Change your password here.</TabsContent>
-                </Tabs>
+      {/* end of top hero */}
+      <div className="flex gap-4 justify-center">
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <div className="grid grid-cols-12 gap-8">
+              {articles.map((article, index) => (
+                <div key={index} className="col-span-12 lg:col-span-4">
+                  <ArticleCard
+                    title={article.title}
+                    author={article.author}
+                    date={article.date}
+                    content={article.content}
+                    imgSrc={article.imgSrc}
+                  />
+                </div>
+              ))}
             </div>
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
 
-
-        </section>
-    )
-}
-
-export default MainPage
+export default MainPage;
